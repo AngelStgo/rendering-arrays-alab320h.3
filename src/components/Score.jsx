8e34f1f0a406c232c469e6dd0b329146f7138837
AngelStgo <euangel.stgo@gmail.com>
2025-03-04 00:05:18 -0500

    last details

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -4,8 +4,16 @@ import learners from "../data/learners";
  function Score() {
     const learnersScore = learners.map((learners, idx) => (
         <div key={idx}>
-            {/* <h3>Date: {learners.scores.date}</h3>
-            <h3>Score:{learners.scores.score}</h3> */}
+            {learners.scores.map((score, index) => (
+              <div key={index} className="scoreDisplay">
+                <h3 >
+                <strong>Date:</strong> {score.date}
+                 <br />
+                  <strong>Score:</strong> {score.score}
+                  </h3> 
+              </div>
+            )
+            )}
         </div>
     ))
   return (
